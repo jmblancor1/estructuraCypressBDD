4ce69160baaf81420d5c9d69ed108b971c5d0a77
Joyce <jm.blancor1@uniandes.edu.co>
2024-09-24 21:14:30 -0500

    Inicializando el repositorio con el proyecto local

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+const cucumber = require('cypress-cucumber-preprocessor').default
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('file:preprocessor', cucumber())
+    },
+    specPattern: "cypress/e2e/Features/*.feature",
+    baseUrl:"https://www.saucedemo.com",
+    supportFile: 'cypress/support/e2e.js',
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout: 120000,
+    video:false,
+    screenshotOnRunFailure:true,
+  },
+});
---
+++ cypress/e2e/Features/Login.feature
@@ -0,0 +1,4 @@
+Feature: Pruebas de Login
+
+  Scenario: Login exitoso
+    Given Usuario va a la pagina principal
